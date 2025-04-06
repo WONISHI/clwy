4e73f5bb94b5c9e4a41f7d1fe1b78dcc50847570
weizhifeng <18797942292@139.com>
2025-04-06 23:35:35 +0800

    feat:调整设置字段和用户接口

---
+++ routes/admin/setting.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const { Setting } = require("../../models");
+const { NotFoundError, success, failure } = require("../../utils/response");
+
+
+// 查询系统设置详情
+router.get("/list/", async function (req, res) {
+  try {
+    // 获取系统设置 ID
+    const setting = await getSetting();
+    success(res, "查询系统设置成功", setting);
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+
+// 更新系统设置
+router.put("/", async function (req, res) {
+  try {
+    const setting = await getSetting();
+    if (setting) {
+      await setting.update(filterBody(req));
+      success(res, "更新系统设置成功", setting);
+    }
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+/**
+ * 公共方法：白名单过滤
+ * @param req
+ * @returns {{copyright: (string|*), icp: (string|string|DocumentFragment|*), name}}
+ */
+function filterBody(req) {
+    return {
+      name: req.body.name,
+      icp: req.body.icp,
+      copyright: req.body.copyright
+    };
+  }
+
+async function getSetting() {
+  const { id } = req.params;
+  const setting = await Setting.findOne(id);
+  if (!setting) {
+    throw new NotFoundError(`ID:${id}的系统设置未找到`);
+  }
+  return setting;
+}
+
+module.exports = router;
---
+++ routes/admin/users.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../../models");
+const { Op } = require("sequelize");
+const { NotFoundError, success, failure } = require("../../utils/response");
+
+// 查询用户列表
+router.get("/list", async function (req, res) {
+  try {
+    const query = req.query;
+    const currentPage = query.currentPage || 1;
+    const pageSize = query.pageSize || 10;
+    const offset = (currentPage - 1) * pageSize;
+    const condition = {
+      order: [["id", "DESC"]],
+      limit: pageSize,
+      offset,
+    };
+    const { count, rows } = await User.findAndCountAll(condition);
+    success(res, "查询用户列表成功", {
+      users: rows,
+      pagination: { currentPage, pageSize, total: count },
+    });
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+// 查询用户详情
+router.get("/list/:id", async function (req, res) {
+  try {
+    // 获取用户 ID
+    const user = await getUser(req);
+    success(res, "查询用户成功", user);
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+// 创建用户
+router.post("/", async function (req, res) {
+  try {
+    const user = await User.create(filterBody(req));
+    success(res, "查询用户成功", user, 201);
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+// 更新用户
+router.put("/:id", async function (req, res) {
+  try {
+    const user = await getUser(req);
+    if (user) {
+      await user.update(filterBody(req));
+      success(res, "更新用户成功", user);
+    }
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+// 模糊搜索
+router.get("/search", async function (req, res) {
+  try {
+    const { title } = req.query;
+    const condition = {
+      order: [["id", "DESC"]],
+    };
+    if (query.email) {
+      condition.where = {
+        email: {
+          [Op.eq]: query.email
+        }
+      };
+    }
+    
+    if (query.username) {
+      condition.where = {
+        username: {
+          [Op.eq]: query.username
+        }
+      };
+    }
+    
+    if (query.nickname) {
+      condition.where = {
+        nickname: {
+          [Op.like]: `%${ query.nickname }%`
+        }
+      };
+    }
+    
+    if (query.role) {
+      condition.where = {
+        role: {
+          [Op.eq]: query.role
+        }
+      };
+    }
+    const users = await User.findAll(condition);
+    success(res, "查询列表成功", { users });
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+/**
+ * 公共方法：白名单过滤
+ * @param req
+ * @returns {{password, role: (number|string|*), introduce: ({type: *}|*), sex: ({allowNull: boolean, type: *, validate: {notNull: {msg: string}, notEmpty: {msg: string}, isIn: {args: [number[]], msg: string}}}|{defaultValue: number, allowNull: boolean, type: *}|*), nickname: (string|*), company: ({type: *}|*), avatar: ({type: *, validate: {isUrl: {msg: string}}}|*), email: (string|*), username}}
+ */
+function filterBody(req) {
+  return {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    nickname: req.body.nickname,
+    sex: req.body.sex,
+    company: req.body.company,
+    introduce: req.body.introduce,
+    role: req.body.role,
+    avatar: req.body.avatar
+  };
+}
+async function getUser(req) {
+  const { id } = req.params;
+  const user = await User.findByPk(id);
+  if (!user) {
+    throw new NotFoundError(`ID:${id}的用户未找到`);
+  }
+  return user;
+}
+
+module.exports = router;
