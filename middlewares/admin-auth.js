0a7ccb6ee8bb4a9ad5af0f53888dc75ac3cd838d
weizhifeng <18797942292@139.com>
2025-04-13 19:21:12 +0800

    feat:新增中间价

---
+++ middlewares/admin-auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+const { UnauthorizedError } = require("../utils/errors");
+const { success, failure } = require("../utils/responses");
+
+module.exports = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    if (!token) {
+      throw new UnauthorizedError("当前接口需要认证才能访问。");
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { userId } = decoded;
+    const user = await User.findByPk(userId);
+    if (!user) {
+      throw new UnauthorizedError("用户不存在。");
+    }
+    if (user.role !== 100) {
+      throw new UnauthorizedError("您没有权限使用当前接口。");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    failure(res, error);
+  }
+};
